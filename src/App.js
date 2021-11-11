01dadf41a8203255c90277af796633f2fe101bf2
Dave Madden <davelearnsstuff@gmail.com>
2021-11-11 11:28:55 -0800

    changed title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
   };
 
   componentDidMount(){
-    console.log('did mount runs')
     axios.get(`https://api.github.com/users/${this.state.input}`)
       .then(resp=> {
         this.setState({
@@ -24,7 +23,6 @@ class App extends React.Component {
       })
   }
   componentDidUpdate(prevProps, prevState){
-    console.log('did update runs')
     if (this.state.user !== prevState.user){
       axios.get(`https://api.github.com/users/${this.state.user.login}/followers`)
       .then(resp=> {
